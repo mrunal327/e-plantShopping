2e53b5734647624cb93bed5af8ccbe26c18140b8
mrunal327 <mrunaldesai2710@gmail.com>
2026-09-23 00:59:56 +0530

    Add App component for landing page

---
+++ src/components/src/components/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+        <p>Welcome to Paradise Nursery</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
