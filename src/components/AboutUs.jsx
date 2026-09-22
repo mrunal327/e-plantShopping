5cf30881612868d6f5abab98ed30ceecb9ab3570
mrunal327 <mrunaldesai2710@gmail.com>
2026-09-23 00:56:56 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are a company dedicated to providing innovative and
+        user-friendly solutions to our customers.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
